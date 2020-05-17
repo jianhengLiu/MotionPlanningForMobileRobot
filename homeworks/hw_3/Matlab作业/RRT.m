0aa89ef9cb2fc28946cfdc60cb2a3c935c26feb7
jianhengLiu <943678231@qq.com>
2020-05-17 15:34:04 +0800

    update RRT matlab hw

--- homeworks/hw_3/Matlab作业/RRT.m
+++ homeworks/hw_3/Matlab作业/RRT.m
@@ -10,7 +10,7 @@ Thr=50;                 %设置目标点阈值
 Delta= 30;              % 设置扩展步长
 %% 建树初始化
 T.v(1).x = x_I;         % T是我们要做的树，v是节点，这里先把起始点加入到T里面来
-T.v(1).y = y_I; 
+T.v(1).y = y_I;
 T.v(1).xPrev = x_I;     % 起始节点的父节点仍然是其本身
 T.v(1).yPrev = y_I;
 T.v(1).dist=0;          %从父节点到该节点的距离，这里可取欧氏距离
@@ -30,32 +30,59 @@ for iter = 1:3000
     x_rand=[];
     %Step 1: 在地图中随机采样一个点x_rand
     %提示：用（x_rand(1),x_rand(2)）表示环境中采样点的坐标
+    x_rand(1) = randi(xL);
+    x_rand(2) = randi(yL);
     
     x_near=[];
-    %Step 2: 遍历树，从树中找到最近邻近点x_near 
+    %Step 2: 遍历树，从树中找到最近邻近点x_near
     %提示：x_near已经在树T里
+    x_near_Idx = 1;
+    x_near_minDis = sqrt((x_rand(1)-T.v(x_near_Idx).x)^2 + (x_rand(2)-T.v(x_near_Idx).y)^2 );
+    for i=1:count
+        dis_temp = sqrt((x_rand(1)-T.v(i).x)^2 + (x_rand(2)-T.v(i).y)^2 );
+        if(dis_temp<x_near_minDis)
+            x_near_minDis = dis_temp;
+            x_near_Idx = i;
+        end
+    end
+    x_near(1) = T.v(x_near_Idx).x;
+    x_near(2) = T.v(x_near_Idx).y;
+    
     
     x_new=[];
     %Step 3: 扩展得到x_new节点
     %提示：注意使用扩展步长Delta
+    x_new = x_near + (x_rand-x_near)./x_near_minDis.*Delta;
     
     %检查节点是否是collision-free
-    %if ~collisionChecking(x_near,x_new,Imp) 
-    %    continue;
-    %end
+    if ~collisionChecking(x_near,x_new,Imp)
+        continue;
+    end
     count=count+1;
     
-    %Step 4: 将x_new插入树T 
+    %Step 4: 将x_new插入树T
     %提示：新节点x_new的父节点是x_near
+    T.v(count).x = x_new(1);         % T是我们要做的树，v是节点，这里先把起始点加入到T里面来
+    T.v(count).y = x_new(2);
+    T.v(count).xPrev = x_near(1);     % 起始节点的父节点仍然是其本身
+    T.v(count).yPrev = x_near(2);
+    T.v(count).dist=Delta;          %从父节点到该节点的距离，这里可取欧氏距离
+    T.v(count).indPrev = x_near_Idx;     %
     
-    %Step 5:检查是否到达目标点附近 
+    %Step 5:检查是否到达目标点附近
     %提示：注意使用目标点阈值Thr，若当前节点和终点的欧式距离小于Thr，则跳出当前for循环
+    if(sqrt((x_new(1)-x_G)^2+(x_new(2)-y_G)^2) < Thr)
+        break;
+    end
     
-   %Step 6:将x_near和x_new之间的路径画出来
-   %提示 1：使用plot绘制，因为要多次在同一张图上绘制线段，所以每次使用plot后需要接上hold on命令
-   %提示 2：在判断终点条件弹出for循环前，记得把x_near和x_new之间的路径画出来
-   
-   pause(0.1); %暂停0.1s，使得RRT扩展过程容易观察
+    %Step 6:将x_near和x_new之间的路径画出来
+    %提示 1：使用plot绘制，因为要多次在同一张图上绘制线段，所以每次使用plot后需要接上hold on命令
+    %提示 2：在判断终点条件弹出for循环前，记得把x_near和x_new之间的路径画出来
+    plot([x_new(1); x_near(1);], [x_new(2); x_near(2)], 'r', 'Linewidth', 3);
+    hold on;
+    
+    
+    pause(0.1); %暂停0.1s，使得RRT扩展过程容易观察
 end
 %% 路径已经找到，反向查询
 if iter < 2000
---
+++ homeworks/hw_3/Matlab作业/Untitled.m
@@ -0,0 +1,3 @@
+startPose = [1,1]
+goalPose = [2,3]
+sum((startPose-goalPose).^2)
